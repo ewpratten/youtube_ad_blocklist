0d6ed922a224463a63188bf8ac24ce0189070f81
Evan Pratten <ewpratten@gmail.com>
2021-01-19 22:59:15 -0500

    readme and gen script

---
+++ README.md
@@ -0,0 +1,25 @@
+# The YouTube ads blocklist project
+
+This is an open project to maintain a list of domain names that serve YouTube ads. The [original](https://gist.github.com/Ewpratten/a25ae63a7200c02c850fede2f32453cf) project only produced a [Pi-hole](https://pi-hole.net/) blocklist, but this new version automatically generates multiple list formats.
+
+## Lists
+
+The following are links to the generated blocklists:
+
+ - [IPV4 / Pi-hole](https://ewpratten.retrylife.ca/youtube_ad_blocklist/hosts.ipv4.txt)
+ - [IPV6 / Pi-hole](https://ewpratten.retrylife.ca/youtube_ad_blocklist/hosts.ipv6.txt)
+ - [Domains](https://ewpratten.retrylife.ca/youtube_ad_blocklist/domains.txt)
+ - [DNSMASQ](https://ewpratten.retrylife.ca/youtube_ad_blocklist/dnsmasq.txt)
+
+## Credits
+
+The following people have contributed to the original project:
+
+ - [@DavisProTv](https://github.com/DavisProTv)
+ - [@lain0](https://github.com/lain0)
+ - [@Totally3D](https://github.com/Totally3D)
+ - [@GitHubb3R](https://github.com/GitHubb3R)
+ - [@fte](https://github.com/fte)
+ - [@gitdine](https://github.com/gitdine)
+ - [@gatinhonico](https://github.com/gatinhonico)
+
---
+++ generator.py
@@ -0,0 +1,60 @@
+"""Blocklist Generator
+
+This script will take a blocklist.txt file and turn it into multiple different blocklist formats
+"""
+
+from typing import *
+import os
+import sys
+
+# Universal header for all generated files
+file_header = """
+                    --- YouTube Ad Blocklist ---
+This project was created and is maintained by: Evan Pratten (@ewpratten)
+More information at: https://github.com/Ewpratten/youtube_ad_blocklist
+"""
+
+
+def generateIPV4Hosts(block_list: List[str]) -> List[str]:
+    return [f"# {line}" for line in file_header.split("\n")]+[f"0.0.0.0 {entry}" for entry in block_list]
+
+def generateIPV6Hosts(block_list: List[str]) -> List[str]:
+    return [f"# {line}" for line in file_header.split("\n")]+[f"::/0 {entry}" for entry in block_list]
+
+def generateDomainsList(block_list: List[str]) -> List[str]:
+    return [f"# {line}" for line in file_header.split("\n")]+[entry for entry in block_list]
+
+def generateDNSMASQList(block_list: List[str]) -> List[str]:
+    return [f"# {line}" for line in file_header.split("\n")]+[f"server=/{entry}/" for entry in block_list]
+
+
+# All generators
+generator_list: dict = {
+    "hosts.ipv4.txt": generateIPV4Hosts,
+    "hosts.ipv6.txt": generateIPV6Hosts,
+    "domains.txt":generateDomainsList,
+    "dnsmasq.txt":generateDNSMASQList
+}
+
+
+def main() -> int:
+
+    # Load the block list to a newline-seperated list
+    entries = []
+    with open("blocklist.txt", "r") as fp:
+        entries = fp.read().split("\n")
+        fp.close()
+
+    # Create the output dir
+    os.makedirs("output", exist_ok=True)
+
+    # Run every generator
+    for gen in generator_list:
+        print(f"Running generator: {gen}")
+        with open(f"output/{gen}", "w") as fp:
+            fp.write("\n".join(generator_list[gen](entries)))
+            fp.close()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
