2d6384e22ba0df0f8a0b2024ed02761366e882db
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-12 08:04:05 -0500

    Updating the AdBlock Plus format

--- generator.py
+++ generator.py
@@ -31,7 +31,7 @@ def generateUnboundList(block_list: List[str]) -> List[str]:
     return [f"# {line}" for line in file_header.split("\n")]+[f"local-zone: \"{entry}\" redirect\nlocal-data: \"{entry} A 127.0.0.1\"" for entry in block_list]
 
 def generateAdblockList(block_list: List[str]) -> List[str]:
-    return ["[Adblock Plus 2.0]"]+[f"! {line}" for line in file_header.split("\n")]+[entry for entry in block_list]
+    return ["[Adblock Plus 2.0]"]+[f"! {line}" for line in file_header.split("\n")]+["||{}^".format(entry) for entry in block_list]
 
 
 # All generators
